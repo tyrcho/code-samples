4de0770e6a3888734b99807d6e4142655e48f5ee
[a518291] Michel Daviot <michel.daviot@atos.net>
2018-04-12 18:03:30 +0200

    :recycle: fix presentation with @spollet (hackyday!)

--- buildLinks.js
+++ buildLinks.js
@@ -1,10 +1,10 @@
 // #tags become markdown links in xmp block
-$("xmp").contents().each(function () {
-    // replace #name with [#name](#name)
-    // and #(long name) with [#long name](#long name)
-    this.textContent = this.textContent.replace(/#\(([^)]+)\)|#(\w+)/g, '[#$1$2](#$1$2)')
-});
 
+function buildLinks(content) {
+    return content.replace(/#\(([^)]+)\)|#(\w+)/g, '[#$1$2](#$1$2)');
+        // replace #name with [#name](#name)
+        // and #(long name) with [#long name](#long name)
+}
 
 // vim replace :
 // :%s/\[#\(\w\+\)\](#\1)/#\1/g
--- buildMasonryToc.js
+++ buildMasonryToc.js
@@ -1,39 +1,43 @@
-$("h1:first").after("<div id='tags' class='masonry'/>");
+function buildMasonryToc() {
 
-$("h1:first").after("<div id='toc'/>");
+    $("h1:first").after("<div id='tags' class='masonry'/>");
 
-$("h2").each(function (i, h) {
-    var name = $(h).text().trim();
-    $(h).before("<br style='clear:both;'/><a href='#" + name + "' id='" + name + "'/>");
-    var a = $(h).prev();
-    $(h).detach();
-    $(h).appendTo($(a));
-});
+    $("h1:first").after("<div id='toc'/>");
 
-$("a:contains('#')").each(function (i, a) {
-    var title = findTitle(a);
-    var name = a.innerText.replace("#", "").replace(" ", "_");
-    // console.log("findTitle(" + a + ") => " + title + " name=" + name);
-    // console.log($("#" + name));
-    if ($("#" + name).length === 0) {
-        $("#tags").append("<div id='" + name + "' class='item'><h2><a href='" + this.innerText + "'>" + this.innerText + "</a></h2> <div id='content-" + name + "'class='content-item'/></div>");
+    $("h2").each(function (i, h) {
+        var name = $(h).text().trim();
+        $(h).before("<br style='clear:both;'/><a href='#" + name + "' id='" + name + "'/>");
+        var a = $(h).prev();
+        $(h).detach();
+        $(h).appendTo($(a));
+    });
+
+    $("a:contains('#')").each(function (i, a) {
+        var title = findTitle(a);
+        var name = a.innerText.replace("#", "").replace(" ", "_");
+        // console.log("findTitle(" + a + ") => " + title + " name=" + name);
+        // console.log($("#" + name));
+        if ($("#" + name).length === 0) {
+            $("#tags").append("<div id='" + name + "' class='item'><h2><a href='" + this.innerText + "'>" + this.innerText + "</a></h2> <div id='content-" + name + "'class='content-item'/></div>");
+        }
+        var h2 = $("#content-" + name);
+        h2.append("<a href='#" + title + "'>" + title + "</a><br/>");
+    });
+
+    $("#content").children("br").each(function (i, a) {
+        var style = i % 2 ? "odd" : "even";
+        $(a).nextUntil("br,h1").addBack().wrapAll("<div class='" + style + "'>");
+    });
+
+    var container = $(".masonry").get(0);
+    new Masonry(container, {itemSelector: '.item'});
+
+
+    function findTitle(elt) {
+        var h2 = $(elt).parent().prevAll("a:first()");
+        return h2.text()
+            ? h2.text()
+            : findTitle($(elt).parent());
     }
-    var h2 = $("#content-" + name);
-    h2.append("<a href='#" + title + "'>" + title + "</a><br/>");
-});
 
-$("#content").children("br").each(function (i, a) {
-    var style = i % 2 ? "odd" : "even";
-    $(a).nextUntil("br,h1").addBack().wrapAll("<div class='" + style + "'>");
-});
-
-var container = $(".masonry").get(0);
-new Masonry(container, {itemSelector: '.item'});
-
-
-function findTitle(elt) {
-    var h2 = $(elt).parent().prevAll("a:first()");
-    return h2.text()
-        ? h2.text()
-        : findTitle($(elt).parent());
 }
